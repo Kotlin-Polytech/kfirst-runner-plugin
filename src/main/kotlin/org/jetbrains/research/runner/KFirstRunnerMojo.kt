04a2c8acd5c34eb412b0e95227b86ea9728d950f
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-08-09 17:30:25 +0300

    Initial commit

---
+++ src/main/kotlin/org/jetbrains/research/runner/KFirstRunnerMojo.kt
@@ -0,0 +1,40 @@
+package org.jetbrains.research.runner
+
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.project.MavenProject
+
+@Mojo(name = "run")
+class KFirstRunnerMojo : AbstractMojo() {
+
+    @Parameter
+    private var packages: List<String> = emptyList()
+    @Parameter
+    private var authorFile: String = "author.name"
+    @Parameter
+    private var ownerFile: String = "owner.name"
+    @Parameter
+    private var resultFile: String = "results.json"
+    @Parameter
+    private var sendToGoogle: Boolean = false
+
+    @Parameter(defaultValue = "\${project}", readonly = true)
+    private lateinit var project: MavenProject
+
+    override fun execute() {
+        val runner = org.jetbrains.research.runner.KFirstRunner()
+
+        val args = RunnerArgs(
+                projectDir = "",
+                classpathPrefix = project.testClasspathElements.map { "file:$it/" },
+                packages = packages,
+                authorFile = authorFile,
+                ownerFile = ownerFile,
+                resultFile = resultFile,
+                sendToGoogle = sendToGoogle
+        )
+
+        runner.run(args)
+    }
+}
